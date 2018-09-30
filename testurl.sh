8a9ddc76d6c8bf2f0b395625775a514a60f74bbd
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-09-30 13:34:07 +0200

    Update testurl.sh

--- testurl.sh
+++ testurl.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "Test URL HTTP Method PUT vulnerable!
+echo "Test URL HTTP Method PUT OPTIONS vulnerable!
 echo "Uso.: sh testurl.sh http://url"
 apt-get install nmap
 nmap -Pn -sV --script http-methods $1
+nikto -h $1
