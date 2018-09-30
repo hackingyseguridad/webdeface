e3dacee4f2eaef8ae8b74f7a7913a7a654ed5725
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-09-30 20:57:59 +0200

    Update httpmethod.sh

--- httpmethod.sh
+++ httpmethod.sh
@@ -1,4 +1,4 @@
-# consulta HTTP method OPTIONS PUT POST HEAD GET status de listado de fqdn en fichero dominios.txt
+# consulta masiva HTTP method OPTIONS PUT POST HEAD GET status de listado de fqdn en fichero dominios.txt
 # www.hackingyseguridad.com
 #!/bin/bash
 for n in `cat dominios.txt`; do echo $n; curl $n -I --silent|grep HTTP; done
