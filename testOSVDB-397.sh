7c65bf745f8d1acd71564d2cc0524e7f553a268f
hackingyseguridad <antonio_taboada@hotmail.es>
2025-04-20 18:17:12 +0200

    Update testOSVDB-397.sh

--- testOSVDB-397.sh
+++ testOSVDB-397.sh
@@ -1,4 +1,4 @@
 echo
 echo " OSVDB-397: Método HTTP (encabezado 'Allow'): el método 'PUT' podría permitir que los clientes guarden archivos en el servidor web.  " 
 echo 
-nikto -h -404code $1
+nikto -h  $1
